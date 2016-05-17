70e4cbd7fd161c969c434daa980643c2636bc2e0
Ben Browning <bbrownin@redhat.com>
2016-05-17 12:13:19 -0400

    Actually check the user/pass against user-supplied env variables

--- verify_user_pass.sh
+++ verify_user_pass.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-EXPECTED_USER="foo"
-EXPECTED_PASSWORD="bar"
-
-if [[ "$username" == "$EXPECTED_USER" && "$password" == "$EXPECTED_PASSWORD" ]]; then
+if [[ "$username" == "$OPENVPN_USER" && "$password" == "$OPENVPN_PASS" ]]; then
     exit 0
 else
     exit 1
